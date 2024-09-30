e873e6d8aac6a5c0b553d8564e03c5113ccead6d
eddiesidgo <124094058+eddiesidgo@users.noreply.github.com>
2024-09-30 15:02:22 -0600

    Pacientes Counter
    En inicio ahora se muestra correctamente el numero de pacientes ingresados

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PacientesService } from '../services/pacientes.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  totalPacientes: number = 0;
+
+  constructor(private pacientesService: PacientesService) {}
+
+  ngOnInit() {
+    this.getPacientesCount();
+  }
+
+  getPacientesCount() {
+    this.pacientesService.getPacientes().subscribe(
+      (data) => {
+        this.totalPacientes = data.length; // Obtén el total de pacientes
+      },
+      (error) => {
+        console.error('Error al obtener el total de pacientes:', error);
+      }
+    );
+  }
 
 }
